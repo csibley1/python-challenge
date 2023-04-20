b3481c52431d62058ce744cc7a41f77ced28c804
csibley1 <55521518+csibley1@users.noreply.github.com>
2023-04-19 20:55:54 -0500

    PyBank completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,46 @@
-print("ugh")
+import os, csv, locale
+locale.setlocale(locale.LC_ALL,'')
+
+# Variables
+change = []
+ctr = 0
+TotalBux = 0
+TotalChange = 0
+AvgChange = 0
+Mo1 = 0
+Mo2 = 0
+ReportHead = "Financial Analysis"
+line = "-" * 28
+
+# Open the csv file
+csv_file = os.path.join("C:\\","Users","steam","Desktop","Starter_Code - Challenge3","PyBank","Resources","budget_data.csv")
+with open(csv_file,'r',encoding='utf') as csvee:
+    bux = csv.DictReader(csvee)
+    for row in bux:
+        # Add Profit/Loss to TotalBux
+        TotalBux += int(row['Profit/Losses'])
+        # Calculate change between months
+        if ctr == 0:
+            Mo1 = int(row['Profit/Losses'])
+        elif ctr == 1:
+            Mo2 = int(row['Profit/Losses'])
+            change.append(Mo2-Mo1)
+        else:
+            Mo1 = Mo2
+            Mo2 = int(row['Profit/Losses'])
+            change.append(Mo2-Mo1)
+        ctr += 1
+
+TotalMonths = ctr
+# Calculate average change
+for chg in change:
+    TotalChange += chg
+AvgChange = round(TotalChange / len(change),2)
+# Show report
+print(ReportHead)
+print(line)
+print(f"Total Months: {TotalMonths}")
+print(f"Total: {locale.currency(TotalBux)}")
+print(f"Average Change: {locale.currency(AvgChange)}")
+print(f"Greatest Increase in Profits: {max(change)}")
+print(f"Greatest Decrease in Profits: {min(change)}")
